c7a5f213abf075acf75e51a6a31eae8da3fbed33
Nicole Smith <nicolelsmith@gmail.com>
2015-10-12 15:57:44 -0400

    adding views

--- ch2/index.php
+++ ch2/index.php
@@ -3,8 +3,8 @@ error_reporting( E_ALL );
 ini_set ( "display_errors" , 1 );
 
 $pageData = new stdClass();
-$pageData->title = "New object-oriented test title";
-$pageData->content = "<h1>Hello from an object</h1>";
+$pageData->title = "Thomas Blom Hansen: Portfolio site";
+$pageData->content = include_once "views/navigation.php";
 $page = include_once "templates/page.php";
 
 echo $page;
