662ff4eb44449fd9f08cec071767ab91f34b08b6
Nicole Smith <nicolelsmith@gmail.com>
2015-11-10 14:08:19 -0500

    finished up chapter 5 adding a lightbox in javascript

--- ch4/js/lightbox.js
+++ ch4/js/lightbox.js
@@ -1,10 +1,42 @@
 window.console.log("Hello from Javascript");
+
 function init() {
   var lightboxElements = "<div id='lightbox'>";
   lightboxElements += "<div id='overlay' class='hidden'></div>";
   lightboxElements += "<img class='hidden' id='big-image' />";
   lightboxElements += "</div>";
   document.querySelector("body").innerHTML += lightboxElements;
+  var bigImage = document.querySelector("#big-image");
+  bigImage.addEventListener("click", toggle, false);
+  prepareThumbs();
+}
+
+function toggle() {
+  window.console.log("show or hide a big image");
+  var clickedImage = event.target;
+  var bigImage = document.querySelector("#big-image");
+  var overlay = document.querySelector("#overlay");
+  bigImage.src = clickedImage.src;
+  if (overlay.getAttribute("class") === "hidden") {
+    overlay.setAttribute("class", "showing");
+    bigImage.setAttribute("class", "showing");
+  } else {
+    overlay.setAttribute("class", "hidden");
+    bigImage.setAttribute("class", "hidden");
+  }
+}
+
+function prepareThumbs(){
+  var liElements = document.querySelectorAll("ul#images li");
+  var i = 0;
+  var image, li;
+  while ( i < liElements.length ){
+    li = liElements[i];
+    li.setAttribute("class", "lightbox");
+    image = li.querySelector("img");
+    image.addEventListener("click", toggle, false);
+    i += 1;
+  }
 }
 
 document.addEventListener("DOMContentLoaded", init, false);
